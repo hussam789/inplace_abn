f92e9ce4e3f6aa2c97ee587cbe2125bdf65dee84
hussam789 <hussam789@gmail.com>
2020-01-29 23:46:59 +0200

    GhostABN

--- inplace_abn/abn.py
+++ inplace_abn/abn.py
@@ -5,90 +5,6 @@ import torch.nn.functional as functional
 
 from .functions import *
 
-class GhostABN(nn.Module):
-    """Activated Ghost Batch Normalization
-
-    This gathers a BatchNorm and an activation function in a single module
-
-    Parameters
-    ----------
-    num_features : int
-        Number of feature channels in the input and output.
-    eps : float
-        Small constant to prevent numerical issues.
-    momentum : float
-        Momentum factor applied to compute running statistics.
-    affine : bool
-        If `True` apply learned scale and shift transformation after normalization.
-    activation : str
-        Name of the activation functions, one of: `relu`, `leaky_relu`, `elu` or `identity`.
-    activation_param : float
-        Negative slope for the `leaky_relu` activation.
-    """
-
-    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True, activation="leaky_relu",
-                 activation_param=0.01, num_splits=4):
-        super(GhostABN, self).__init__()
-        self.num_features = num_features
-        self.affine = affine
-        self.eps = eps
-        self.momentum = momentum
-        self.activation = activation
-        self.activation_param = activation_param
-        self.num_splits = num_splits
-        if self.affine:
-            self.weight = nn.Parameter(torch.ones(num_features*self.num_splits))
-            self.bias = nn.Parameter(torch.zeros(num_features*self.num_splits))
-        else:
-            self.register_parameter('weight', None)
-            self.register_parameter('bias', None)
-        self.register_buffer('running_mean', torch.zeros(num_features*self.num_splits))
-        self.register_buffer('running_var', torch.ones(num_features*self.num_splits))
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        nn.init.constant_(self.running_mean, 0)
-        nn.init.constant_(self.running_var, 1)
-        if self.affine:
-            nn.init.constant_(self.weight, 1)
-            nn.init.constant_(self.bias, 0)
-
-    def forward(self, x):
-        N, C, H, W = input.shape
-        x = functional.batch_norm(
-                input.view(-1, C*self.num_splits, H, W), self.running_mean, self.running_var, 
-                self.weight.repeat(self.num_splits), self.bias.repeat(self.num_splits),
-                True, self.momentum, self.eps).view(N, C, H, W) 
-#         x = functional.batch_norm(x, self.running_mean, self.running_var, self.weight, self.bias,
-#                                   self.training, self.momentum, self.eps)
-
-        if self.activation == "relu":
-            return functional.relu(x, inplace=True)
-        elif self.activation == "leaky_relu":
-            return functional.leaky_relu(x, negative_slope=self.activation_param, inplace=True)
-        elif self.activation == "elu":
-            return functional.elu(x, alpha=self.activation_param, inplace=True)
-        elif self.activation == "identity":
-            return x
-        else:
-            raise RuntimeError("Unknown activation function {}".format(self.activation))
-
-    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys,
-                              error_msgs):
-        # Post-Pytorch 1.0 models using standard BatchNorm have a "num_batches_tracked" parameter that we need to ignore
-        num_batches_tracked_key = prefix + "num_batches_tracked"
-        if num_batches_tracked_key in state_dict:
-            del state_dict[num_batches_tracked_key]
-
-        super(GhostABN, self)._load_from_state_dict(state_dict, prefix, local_metadata, strict, missing_keys,
-                                               error_msgs, unexpected_keys)
-
-    def extra_repr(self):
-        rep = '{num_features}, eps={eps}, momentum={momentum}, affine={affine}, activation={activation}'
-        if self.activation in ["leaky_relu", "elu"]:
-            rep += '[{activation_param}]'
-        return rep.format(**self.__dict__)
-
 class ABN(nn.Module):
     """Activated Batch Normalization
 
@@ -167,6 +83,89 @@ class ABN(nn.Module):
             rep += '[{activation_param}]'
         return rep.format(**self.__dict__)
 
+class GhostABN(nn.Module):
+    """Activated Ghost Batch Normalization
+
+    This gathers a BatchNorm and an activation function in a single module
+
+    Parameters
+    ----------
+    num_features : int
+        Number of feature channels in the input and output.
+    eps : float
+        Small constant to prevent numerical issues.
+    momentum : float
+        Momentum factor applied to compute running statistics.
+    affine : bool
+        If `True` apply learned scale and shift transformation after normalization.
+    activation : str
+        Name of the activation functions, one of: `relu`, `leaky_relu`, `elu` or `identity`.
+    activation_param : float
+        Negative slope for the `leaky_relu` activation.
+    """
+
+    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True, activation="leaky_relu",
+                 activation_param=0.01, num_splits=4):
+        super(GhostABN, self).__init__()
+        self.num_features = num_features
+        self.affine = affine
+        self.eps = eps
+        self.momentum = momentum
+        self.activation = activation
+        self.activation_param = activation_param
+        self.num_splits = num_splits
+        if self.affine:
+            self.weight = nn.Parameter(torch.ones(num_features*self.num_splits))
+            self.bias = nn.Parameter(torch.zeros(num_features*self.num_splits))
+        else:
+            self.register_parameter('weight', None)
+            self.register_parameter('bias', None)
+        self.register_buffer('running_mean', torch.zeros(num_features*self.num_splits))
+        self.register_buffer('running_var', torch.ones(num_features*self.num_splits))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.constant_(self.running_mean, 0)
+        nn.init.constant_(self.running_var, 1)
+        if self.affine:
+            nn.init.constant_(self.weight, 1)
+            nn.init.constant_(self.bias, 0)
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        x = functional.batch_norm(
+                x.view(-1, C*self.num_splits, H, W), self.running_mean, self.running_var, 
+                self.weight.repeat(self.num_splits), self.bias.repeat(self.num_splits),
+                True, self.momentum, self.eps).view(N, C, H, W) 
+#         x = functional.batch_norm(x, self.running_mean, self.running_var, self.weight, self.bias,
+#                                   self.training, self.momentum, self.eps)
+
+        if self.activation == "relu":
+            return functional.relu(x, inplace=True)
+        elif self.activation == "leaky_relu":
+            return functional.leaky_relu(x, negative_slope=self.activation_param, inplace=True)
+        elif self.activation == "elu":
+            return functional.elu(x, alpha=self.activation_param, inplace=True)
+        elif self.activation == "identity":
+            return x
+        else:
+            raise RuntimeError("Unknown activation function {}".format(self.activation))
+
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys,
+                              error_msgs):
+        # Post-Pytorch 1.0 models using standard BatchNorm have a "num_batches_tracked" parameter that we need to ignore
+        num_batches_tracked_key = prefix + "num_batches_tracked"
+        if num_batches_tracked_key in state_dict:
+            del state_dict[num_batches_tracked_key]
+
+        super(GhostABN, self)._load_from_state_dict(state_dict, prefix, local_metadata, strict, missing_keys,
+                                               error_msgs, unexpected_keys)
+
+    def extra_repr(self):
+        rep = '{num_features}, eps={eps}, momentum={momentum}, affine={affine}, activation={activation}'
+        if self.activation in ["leaky_relu", "elu"]:
+            rep += '[{activation_param}]'
+        return rep.format(**self.__dict__)
 
 class InPlaceABN(ABN):
     """InPlace Activated Batch Normalization
@@ -195,6 +194,34 @@ class InPlaceABN(ABN):
         return inplace_abn(x, self.weight, self.bias, self.running_mean, self.running_var,
                            self.training, self.momentum, self.eps, self.activation, self.activation_param)
 
+class InPlaceGhostABN(GhostABN):
+    """InPlace Activated Ghost Batch Normalization
+
+    Parameters
+    ----------
+    num_features : int
+        Number of feature channels in the input and output.
+    eps : float
+        Small constant to prevent numerical issues.
+    momentum : float
+        Momentum factor applied to compute running statistics.
+    affine : bool
+        If `True` apply learned scale and shift transformation after normalization.
+    activation : str
+        Name of the activation functions, one of: `leaky_relu`, `elu` or `identity`.
+    activation_param : float
+        Negative slope for the `leaky_relu` activation.
+    """
+
+    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True, activation="leaky_relu",
+                 activation_param=0.01, num_splits=4):
+        super(InPlaceABN, self).__init__(num_features, eps, momentum, affine, activation, activation_param, num_splits)
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        return inplace_abn(x.view(-1, C*self.num_splits, H, W), self.weight.repeat(self.num_splits),
+                           self.bias.repeat(self.num_splits), self.running_mean, self.running_var,
+                           self.training, self.momentum, self.eps, self.activation, self.activation_param).view(N, C, H, W) 
 
 class InPlaceABNSync(ABN):
     """InPlace Activated Batch Normalization with cross-GPU synchronization
